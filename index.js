664ced64ba32eb0f8c055274e2b0db6bb3fb8284
Unknown <niehau@gmail.com>
2018-06-03 21:38:27 +0200

    added Jquery for smooth scroll to anchor

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $( "a.scrollLink" ).click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+    });
+});
