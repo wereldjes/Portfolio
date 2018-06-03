f7c57ee7dd8cafe5932576d23331023a83e12459
Unknown <niehau@gmail.com>
2018-06-03 22:08:48 +0200

    added back to top button

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
         event.preventDefault();
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
     });
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scroll-to-top').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
 });
+
